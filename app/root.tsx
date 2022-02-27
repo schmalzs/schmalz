841d5715f841c91f5b1b23cf929ce765fe0dd7a9
Scott Schmalz <scott.schmalz@gmail.com>
2022-02-26 22:04:52 -0500

    Add favicons and tailwind

--- app/root.tsx
+++ app/root.tsx
@@ -4,12 +4,36 @@ import {
   Meta,
   Outlet,
   Scripts,
-  ScrollRestoration
-} from "remix";
-import type { MetaFunction } from "remix";
+  ScrollRestoration,
+} from 'remix';
+import type { LinksFunction, MetaFunction } from 'remix';
+import styles from './tailwind.css';
+
+export const links: LinksFunction = () => [
+  { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+  {
+    rel: 'icon',
+    type: 'image/png',
+    sizes: '32x32',
+    href: '/favicon-32x32.png',
+  },
+  {
+    rel: 'icon',
+    type: 'image/png',
+    sizes: '16x16',
+    href: '/favicon-16x16.png',
+  },
+  { rel: 'manifest', href: '/site.webmanifest' },
+  { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#5bbad5' },
+  {
+    rel: 'stylesheet',
+    href: 'https://fonts.googleapis.com/css?family=Open+Sans:400,700|Source+Code+Pro:400,700',
+  },
+  { rel: 'stylesheet', href: styles },
+];
 
 export const meta: MetaFunction = () => {
-  return { title: "New Remix App" };
+  return { title: 'Scott Schmalz - Full Stack Engineer' };
 };
 
 export default function App() {
